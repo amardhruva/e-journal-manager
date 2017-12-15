25661869203f52ffb6ace7df2dcc9646f50a96a5
Amardhruva Prabhu <flashrocket@users.noreply.github.com>
2017-12-15 15:46:06 +0530

    Fix Bug With author

--- papermanager/models.py
+++ papermanager/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Paper(models.Model):
     name=models.CharField(max_length=100)
     description=models.TextField(blank=True)
-    author=User
+    author=models.ForeignKey(User)
     public=models.BooleanField(default=False)
     def __str__(self):
         return self.name
