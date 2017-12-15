80d20c0a0edda742ea36e5a4a97ec522de299889
Amardhruva Prabhu <flashrocket@users.noreply.github.com>
2017-12-15 17:27:09 +0530

    Add Slug to paper model

--- papermanager/models.py
+++ papermanager/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from slugger.fields import AutoSlugField
 
 # Create your models here.
 class Paper(models.Model):
@@ -7,6 +8,8 @@ class Paper(models.Model):
     description=models.TextField(blank=True)
     author=models.ForeignKey(User)
     public=models.BooleanField(default=False)
+    slug = AutoSlugField(populate_from='name',unique=True)
+
     def __str__(self):
         return self.name
 
