a92f9b98b2f2b73b0f324421292be662e9b5f47f
Amardhruva Prabhu <flashrocket@users.noreply.github.com>
2017-12-15 13:24:24 +0530

    Added models for paper submissions

--- papermanager/models.py
+++ papermanager/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Paper(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(blank=True)
+    author=User
+    public=models.BooleanField(default=False)
+
+class PaperVersion(models.Model):
+    SUBMISSION_CHOICES=(
+        ("latex","Lamport TeX (LaTeX)"),
+        ("pdf","Portable Document Format (PDF)"),
+    )
+    name=models.CharField(max_length=20)
+    submissionType=models.CharField(max_length=7,choices=SUBMISSION_CHOICES)
+    submissionDate=models.DateField(auto_now_add=True)
+
+class PaperFiles(models.Model):
+    filename=models.CharField(max_length=255)
+    filedata=models.BinaryField()
