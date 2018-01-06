bb016a0b52b037e866b95d7be87f939e37a9885b
Amardhruva Prabhu <flashrocket@users.noreply.github.com>
2018-01-06 16:38:05 +0530

    Added reviewstatus model

--- paperreviewer/models.py
+++ paperreviewer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from papermanager.models import PaperVersion
+from django.contrib.auth.models import User
+
+REVIEW_STATUS_CHOICES=(
+    ('P',"Review Pending"),
+    ('Y',"Accepted"),
+    ('N',"Rejected"),
+)
 
 # Create your models here.
+class ReviewStatus(models.Model):
+    paperversion=models.OneToOneField(PaperVersion)
+    status=models.CharField(max_length=1, choices=REVIEW_STATUS_CHOICES)
+
