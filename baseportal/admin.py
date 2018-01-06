0bf54143783c9180f01bc5684e9a9d9789cd0731
Amardhruva Prabhu <flashrocket@users.noreply.github.com>
2018-01-06 18:12:03 +0530

    Moved admin registry ot correct apps

--- baseportal/admin.py
+++ baseportal/admin.py
@@ -2,5 +2,3 @@ from django.contrib import admin
 from papermanager.models import Paper, PaperVersion
 
 # Register your models here.
-admin.site.register(Paper)
-admin.site.register(PaperVersion)
--- papermanager/admin.py
+++ papermanager/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from papermanager.models import Paper, PaperVersion
 
 # Register your models here.
+admin.site.register(Paper)
+admin.site.register(PaperVersion)
