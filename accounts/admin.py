42c2033854430cdf9a0a2c2310e565c36b075d40
Amardhruva Prabhu <flashrocket@users.noreply.github.com>
2018-01-08 13:30:27 +0530

    Added activate/deactivate user to admin interface

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,11 +6,23 @@ from django.contrib.auth.models import User
 # Register your models here.
 class ProfileInline(admin.StackedInline):
     model = UserType
+    
+
 
 class IspUserAdmin(UserAdmin):
     list_display = ('username', 'first_name', 'last_name', 'email', 'is_staff', 'is_superuser', 'is_active','usertype')
     list_filter = ('usertype__type','is_active','date_joined', 'last_login', 'is_staff', 'is_superuser', )
     inlines = (ProfileInline,)
+    
+    actions= ['activate_user', 'deactivate_user']
+    
+    def activate_user(self, request, queryset):
+        queryset.update(is_active=True)
+    activate_user.short_description = "Activate Users"
+    
+    def deactivate_user(self, request, queryset):
+        queryset.update(is_active=False)
+    deactivate_user.short_description = "Deactivate Users"
 
 admin.site.unregister(User)
 admin.site.register(User, IspUserAdmin)
