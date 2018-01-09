e5acf7f135ab84007e5c41e3daf3ed1abdcd9135
Amardhruva Prabhu <flashrocket@users.noreply.github.com>
2018-01-09 11:53:43 +0530

    Added model for published files

--- baseportal/models.py
+++ baseportal/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from papermanager.models import Paper
 
 # Create your models here.
+class FinalPaper(models.Model):
+    publishedDate=models.DateTimeField(auto_now_add=True)
+    paper=models.OneToOneField(Paper)
+    whitePaper=models.FileField(upload_to='uploads/%Y-%m-%d/')
